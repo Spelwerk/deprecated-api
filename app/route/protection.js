1abea7f6d666bb07d46ec1c065e0a551151a85df
princejonn <jonn@jonn.nu>
2016-09-16 13:50:51 +0200

    protection added protection changed installation/

---
+++ app/route/protection.js
@@ -0,0 +1,49 @@
+var rest = require('./../rest');
+
+module.exports = function(pool, router, table, path) {
+    path = path || '/' + table;
+
+    var query = 'SELECT ' +
+        'protection.id, ' +
+        'protection.name, ' +
+        'protection.description, ' +
+        'protection.price, ' +
+        'protection.created, ' +
+        'protection.deleted, ' +
+        'protection.protectiontype_id, ' +
+        'protectiontype.name AS protectiontype_name, ' +
+        'protection.attribute_id, ' +
+        'attribute.name AS attribute_name ' +
+        'FROM protection ' +
+        'LEFT JOIN protectiontype ON protectiontype.id = protection.protectiontype_id ' +
+        'LEFT JOIN attribute ON attribute.id = protection.give_attribute_id';
+
+    router.get(path + '/help', function(req, res) {
+        rest.HELP(pool, req, res, table);
+    });
+
+    router.get(path, function(req, res) {
+        rest.QUERY(pool, req, res, query, null);
+    });
+
+    router.get(path + '/id/:id', function(req, res) {
+        var call = query + ' WHERE protection.id = ?';
+        rest.QUERY(pool, req, res, call, [req.params.id]);
+    });
+
+    router.post(path, function(req, res) {
+        rest.POST(pool, req, res, table, req.body);
+    });
+
+    router.put(path, function(req, res) {
+        rest.INSERT(pool, req, res, table, req.body);
+    });
+
+    router.put(path + '/id/:id', function(req, res) {
+        rest.PUT(pool, req, res, table, req.body, 'id');
+    });
+
+    router.delete(path + '/id/:id', function(req, res) {
+        rest.DELETE(pool, req, res, table, 'id');
+    });
+};
